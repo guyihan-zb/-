eb1bfe92b8de15df64170626fdb0a317df01f7b0
ZhangBo <2092332348@qq.com>
2020-12-01 17:14:36 +0800

    加1

---
+++ 20201201/2.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main2()
+{
+    using namespace std;
+    int a, b;
+        a = 10;
+        b = ++a * 10;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl;
+    return 0;
+}
---
+++ 20201201/2.3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main3()
+{
+	using namespace std;
+	int carrots;
+	carrots = 25;
+	cout << " i have "  << carrots;
+	cout << "  carrots."<<endl;
+	carrots = carrots - 1;
+	cout << "oh.now i have" << carrots <<"  carrots"<< endl;
+	return 0;
+}
